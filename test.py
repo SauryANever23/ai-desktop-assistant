50ab3423f06db1ba17170b52e118429d8c9a1d00
SauryANever23 <sauryajha9841@gmail.com>
2025-05-26 20:28:11 +0545

    updating the test.py

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
-from pydbus import SystemBus
-import time
+def scan_services():
+ 
+   say("connecting to bluetooth...")
+ 
+   devices = bluetooth.discover_devices(lookup_names = True)
+ 
+   number_of_devices = len(devices)
+ 
+   print(number_of_devices, "devices found")
+ 
+   for addr,name in devices:
+ 
+      print("\n")
+ 
+      print("Device Name: %s" % (name))
+ 
+      print("Device MAC Address: %s" % (addr))
+ 
+      print("Services Found:")
+ 
+      services = bluetooth.find_service(address=addr)
+ 
+      if len(services) <=0:
+ 
+          print("zero services found on", addr)
+ 
+      else:
+ 
+          for serv in services:
+ 
+              print(serv['name'])
+ 
+      print("\n")
 
-DEVICE_MAC = "48:f1:7f:f0:e5:6f"  # Replace with your device MAC
-
-bus = SystemBus()
-manager = bus.get("org.bluez", "/")
-
-# Get Bluetooth adapter (usually hci0)
-adapter_path = "/org/bluez/hci0"
-adapter = bus.get("org.bluez", adapter_path)
-
-# Start discovery
-adapter.StartDiscovery()
-print("Discovering devices...")
-time.sleep(5)  # Allow time for discovery
-adapter.StopDiscovery()
-
-# Format device path
-device_path = f"{adapter_path}/dev_{DEVICE_MAC.replace(':', '_')}"
-device = bus.get("org.bluez", device_path)
-
-# Trust the device
-device.Trusted = True
-
-# Pair the device if not paired
-if not device.Paired:
-    print("Pairing...")
-    device.Pair()
-    print("Paired.")
-
-# Connect
-print("Connecting...")
-device.Connect()
-print("Connected.")
+    return ()
