3312427e7727280e92319a5d6b2f0177dbc01cfe
SauryANever23 <sauryajha9841@gmail.com>
2025-05-26 20:27:31 +0545

    updating the main.py file

--- main.py
+++ main.py
@@ -38,9 +38,49 @@ def openWebsites(cmd):
     webbrowser.open(site)
 
 
+def scan_services():
+ 
+   say("connecting to bluetooth...")
+ 
+   devices = bluetooth.discover_devices(lookup_names = True)
+ 
+   number_of_devices = len(devices)
+ 
+   print(number_of_devices, "devices found")
+ 
+   for addr,name in devices:
+ 
+      print("\n")
+ 
+      print("Device Name: %s" % (name))
+ 
+      print("Device MAC Address: %s" % (addr))
+ 
+      print("Services Found:")
+ 
+      services = bluetooth.find_service(address=addr)
+ 
+      if len(services) <=0:
+ 
+          print("zero services found on", addr)
+ 
+      else:
+ 
+          for serv in services:
+ 
+              print(serv['name'])
+ 
+      print("\n")
+
+    return ()
+ 
+ 
+main():
+    say("Jarvis Found")
+
 if __name__ == '__main__':
     print("listening...")
     t = takeCommand()
-    openWebsites(t)
+
 
     
