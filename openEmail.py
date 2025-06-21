c29d90bd9cc1b9931a36d9fac26d8cdcc274600a
SauryANever23 <sauryajha9841@gmail.com>
2025-06-21 12:00:42 +0545

    adding security using TLS

--- openEmail.py
+++ openEmail.py
@@ -6,7 +6,16 @@ class SendEmail():
     """This class is used to send emails"""
     
     @staticmethod
-    def send(message: str, subject: str, reciever):
+    def send(message: str, subject: str, reciever: str) -> None:
+        payload = EmailMessage()
+        msg.set_content(f"{message}") # sending the content 
+        msg["Subject"] = subject
+        msg["From"] = os.environ["EMAIL_ID"]
+        msg["To"] = reciever
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 587) as s:
+            s.starttls()
+            
         
 
 def main():
