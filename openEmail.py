6c8aa869c5d9f65752756830d28b5becf5660ebf
SauryANever23 <sauryajha9841@gmail.com>
2025-06-21 11:36:32 +0545

    wrote the openEmail but need to test

--- openEmail.py
+++ openEmail.py
@@ -1,7 +1,35 @@
 import smtplib 
-import subprocesses 
+import os
 
 class SendEmail():
     """This class is used to send emails"""
-    def __init__(self):
-        self.s = smptlib.SMPT('smpt.gmail.com', 587)
+    
+    @staticmethod
+    def send(message, reciever):
+        # create an smtp session
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # start TLS for security
+        s.starttls()
+
+        # Authentication 
+        s.login(os.environ["EMAIL_ID"], os.environ["EMAIL_ID_PASS"])
+
+        # message that is to be sent 
+        payload = f"{message}"
+
+        s.sendmail(os.environ["EMAIL_ID"], reciever, payload)
+
+        s.quit()
+
+def main():
+    SendEmail.send("I am sending this using SMTPlib", "mukeshkumarjha98@gmail.com")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
