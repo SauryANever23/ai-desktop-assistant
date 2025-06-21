86fca864af854dc7c06789e09240d23b95f11cbd
SauryANever23 <sauryajha9841@gmail.com>
2025-06-21 12:03:56 +0545

    the code is complete testing is left

--- openEmail.py
+++ openEmail.py
@@ -8,18 +8,23 @@ class SendEmail():
     @staticmethod
     def send(message: str, subject: str, reciever: str) -> None:
         payload = EmailMessage()
-        msg.set_content(f"{message}") # sending the content 
-        msg["Subject"] = subject
-        msg["From"] = os.environ["EMAIL_ID"]
-        msg["To"] = reciever
+        payload.set_content(f"{message}") # sending the content 
+        payload["Subject"] = subject
+        payload["From"] = os.environ["EMAIL_ID"]
+        payload["To"] = reciever
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 587) as s:
-            s.starttls()
+            s.starttls() # Transport layer security 
+            s.login(os.environ["EMAIL_ID"], os.environ["EMAIL_ID_PASS"]) # Logging in 
+            s.send_message(payload)
             
-        
 
 def main():
-    SendEmail.send("I am sending this using SMTPlib", "mukeshkumarjha98@gmail.com")
+    message = """
+    ## I want you to make my computer work
+    - For that reason lets go make it, after lunch
+    """
+    SendEmail.send(message, "Computer" "mukeshkumarjha98@gmail.com")
 
 if __name__ == '__main__':
     main()
